76af0e3b27611c08dc3a87a09c4135e695477727
meenjoon <zqudwnsz@naver.com>
2023-09-12 17:40:02 +0900

    feat: 한글명/현재가/전일대비/거래대금 정렬 레이아웃 구현

--- app/src/main/java/com/mbj/upbit/MainActivity.kt
+++ app/src/main/java/com/mbj/upbit/MainActivity.kt
@@ -3,11 +3,28 @@ package com.mbj.upbit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mbj.upbit.model.CoinInfo
+import com.mbj.upbit.model.CoinInfo.Companion.coinInfoItems
 import com.mbj.upbit.ui.theme.UpbitTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    FilterCoinInfo(coinInfoItems)
                 }
             }
         }
@@ -28,17 +45,102 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FilterCoinInfo(coinInfoItems: List<CoinInfo>) {
+    Column {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Top,
+            modifier = Modifier
+                .height(20.dp)
+                .fillMaxSize()
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(start = 20.dp)) {
+                Text(
+                    text = "한글명",
+                    fontSize = 14.sp,
+                )
+                Column {
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowRight,
+                        contentDescription = null,
+                        modifier = Modifier.size(8.dp)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowLeft,
+                        contentDescription = null,
+                        modifier = Modifier.size(8.dp)
+                    )
+                }
+            }
+            Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(start = 50.dp)) {
+                Text(
+                    text = "현재가",
+                    fontSize = 14.sp,
+                )
+                Column {
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowUp,
+                        contentDescription = null,
+                        modifier = Modifier.size(8.dp)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowDown,
+                        contentDescription = null,
+                        modifier = Modifier.size(8.dp)
+                    )
+                }
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "전일 대비",
+                    fontSize = 14.sp,
+                )
+                Column {
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowUp,
+                        contentDescription = null,
+                        modifier = Modifier.size(8.dp)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowDown,
+                        contentDescription = null,
+                        modifier = Modifier.size(8.dp)
+                    )
+                }
+            }
+            Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(end = 20.dp)) {
+                Text(
+                    text = "거래대금",
+                    fontSize = 14.sp,
+                )
+                Column {
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowUp,
+                        contentDescription = null,
+                        modifier = Modifier.size(8.dp)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.KeyboardArrowDown,
+                        contentDescription = null,
+                        modifier = Modifier.size(8.dp)
+                    )
+                }
+            }
+        }
+        Divider(
+            color = Color.DarkGray,
+            thickness = 0.2.dp,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun FilterCoinInfoPreview() {
     UpbitTheme {
-        Greeting("Android")
+        FilterCoinInfo(coinInfoItems)
     }
 }
