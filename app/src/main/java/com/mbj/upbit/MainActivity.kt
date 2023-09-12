992bc834bfc298a0890bc5807ad1978372d2ad97
meenjoon <zqudwnsz@naver.com>
2023-09-12 17:44:11 +0900

    feat: 코인 정보를 보여주는 LazyColumn 레이아웃 구현

--- app/src/main/java/com/mbj/upbit/MainActivity.kt
+++ app/src/main/java/com/mbj/upbit/MainActivity.kt
@@ -6,20 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,8 +62,10 @@ fun FilterCoinInfo(coinInfoItems: List<CoinInfo>) {
                 .height(20.dp)
                 .fillMaxSize()
         ) {
-            Row(verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(start = 20.dp)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(start = 20.dp)
+            ) {
                 Text(
                     text = "한글명",
                     fontSize = 14.sp,
@@ -73,8 +83,10 @@ fun FilterCoinInfo(coinInfoItems: List<CoinInfo>) {
                     )
                 }
             }
-            Row(verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(start = 50.dp)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(start = 50.dp)
+            ) {
                 Text(
                     text = "현재가",
                     fontSize = 14.sp,
@@ -110,8 +122,10 @@ fun FilterCoinInfo(coinInfoItems: List<CoinInfo>) {
                     )
                 }
             }
-            Row(verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.padding(end = 20.dp)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(end = 20.dp)
+            ) {
                 Text(
                     text = "거래대금",
                     fontSize = 14.sp,
@@ -133,8 +147,103 @@ fun FilterCoinInfo(coinInfoItems: List<CoinInfo>) {
         Divider(
             color = Color.DarkGray,
             thickness = 0.2.dp,
+            modifier = Modifier.padding(top = 4.dp)
         )
+        LazyColumn {
+            items(coinInfoItems) { item ->
+                CoinInfoItem(item)
+            }
+        }
     }
+}
+
+@Composable
+fun CoinInfoItem(coinInfo: CoinInfo) {
+    Spacer(modifier = Modifier.padding(top = 2.dp))
+    Row(
+        horizontalArrangement = Arrangement.Start,
+
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .height(50.dp)
+            .fillMaxSize()
+            .padding(start = 10.dp, end = 10.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .height(60.dp)
+                .width(120.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Star,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(20.dp)
+            )
+            Spacer(modifier = Modifier.padding(end = 4.dp))
+            Column(
+                horizontalAlignment = Alignment.Start,
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = coinInfo.name,
+                        fontSize = 12.sp,
+                        modifier = Modifier.padding(end = 4.dp)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.Info,
+                        contentDescription = null
+                    )
+                }
+                Text(
+                    text = coinInfo.coinNameAbbreviation,
+                    fontSize = 8.sp,
+                )
+            }
+        }
+        Column(
+            horizontalAlignment = Alignment.End,
+            modifier = Modifier.width(60.dp),
+        ) {
+            Text(
+                text = coinInfo.currentPrice.toString(),
+                fontSize = 12.sp,
+            )
+        }
+        Spacer(modifier = Modifier.padding(end = 40.dp))
+
+        Column(
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = "${coinInfo.percentComparedPreviousDay}%",
+                fontSize = 12.sp,
+            )
+            Text(
+                text = "${coinInfo.moneyComparedPreviousDay}",
+                fontSize = 10.sp,
+            )
+        }
+        Spacer(modifier = Modifier.padding(end = 20.dp))
+
+        Column(
+            horizontalAlignment = Alignment.End,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "${coinInfo.transactionPayment}백만",
+                fontSize = 12.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+    Spacer(modifier = Modifier.padding(bottom = 2.dp))
+    Divider(
+        color = Color.DarkGray,
+        thickness = 0.2.dp,
+    )
 }
 
 @Preview(showBackground = true)
@@ -142,5 +251,22 @@ fun FilterCoinInfo(coinInfoItems: List<CoinInfo>) {
 fun FilterCoinInfoPreview() {
     UpbitTheme {
         FilterCoinInfo(coinInfoItems)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CoinInfoItemPreview() {
+    UpbitTheme {
+        CoinInfoItem(
+            coinInfo = CoinInfo(
+                name = "비트토렌트",
+                coinNameAbbreviation = "BTT/KRW",
+                currentPrice = 0.0006,
+                percentComparedPreviousDay = 0.00,
+                moneyComparedPreviousDay = 0.0000,
+                transactionPayment = 171
+            )
+        )
     }
 }
