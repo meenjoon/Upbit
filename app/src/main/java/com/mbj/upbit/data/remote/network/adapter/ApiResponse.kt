29e2bd237e828c37f187967e6ce92873b24f22d1
meenjoon <zqudwnsz@naver.com>
2023-09-15 18:42:38 +0900

    feat: ApiCallAdapter 구현

---
+++ app/src/main/java/com/mbj/upbit/data/remote/network/adapter/ApiResponse.kt
@@ -0,0 +1,9 @@
+package com.mbj.upbit.data.remote.network.adapter
+
+sealed interface ApiResponse<T : Any>
+
+class ApiResultSuccess<T : Any>(val data: T) : ApiResponse<T>
+
+class ApiResultError<T : Any>(val code: Int, val message: String) : ApiResponse<T>
+
+class ApiResultException<T : Any>(val throwable: Throwable) : ApiResponse<T>
