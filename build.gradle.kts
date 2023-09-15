c4df5b6b553e1d667af690a13d9cac0600de3cfe
meenjoon <zqudwnsz@naver.com>
2023-09-15 18:44:44 +0900

    chore: enum class 오류 해결을 위한 r8 종속성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
 
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://storage.googleapis.com/r8-releases/raw")
+        }
+    }
+
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
+        classpath("com.android.tools:r8:8.2.16-dev")
     }
 }
 
