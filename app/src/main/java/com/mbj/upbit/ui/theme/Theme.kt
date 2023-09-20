2651a105fd970edfd0ad35105a528ee7c7474fa3
meenjoon <zqudwnsz@naver.com>
2023-09-21 01:18:29 +0900

    feat: 앱 statusBar 색상 설정( 라이트모드, 다크모드 )

--- app/src/main/java/com/mbj/upbit/ui/theme/Theme.kt
+++ app/src/main/java/com/mbj/upbit/ui/theme/Theme.kt
@@ -1,11 +1,15 @@
 package com.mbj.upbit.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import com.mbj.upbit.ui.theme.CustomColors.Companion.Navy300
 
 private val DarkColorPalette = darkColors(
@@ -40,6 +44,20 @@ fun UpbitTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable 
         DarkTypography
     } else {
         LightTypography
+    }
+
+    val statusBarColor = if (darkTheme) {
+        Navy300
+    } else {
+        Color.White
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = statusBarColor.toArgb()
+        }
     }
 
     MaterialTheme(
