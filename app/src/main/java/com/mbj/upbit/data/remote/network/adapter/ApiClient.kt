fba5735f4807818694b96153d5c6cd78419e7e69
meenjoon <zqudwnsz@naver.com>
2023-10-01 17:42:02 +0900

    fix: 불필요한 코드 삭제

--- app/src/main/java/com/mbj/upbit/data/remote/network/adapter/ApiClient.kt
+++ app/src/main/java/com/mbj/upbit/data/remote/network/adapter/ApiClient.kt
@@ -14,29 +14,4 @@ interface ApiClient {
 
     @GET("v1/market/all")
     suspend fun getAllMarketInfo(): ApiResponse<List<CoinInfo>>
-
-    companion object {
-
-        fun create(): ApiClient {
-            val logger = HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            }
-
-            val client = OkHttpClient.Builder()
-                .addInterceptor(logger)
-                .build()
-
-            val moshi: Moshi = Moshi.Builder()
-                .add(KotlinJsonAdapterFactory())
-                .build()
-
-            return Retrofit.Builder()
-                .baseUrl("https://api.upbit.com/")
-                .client(client)
-                .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .addCallAdapterFactory(ApiCallAdapterFactory.create())
-                .build()
-                .create(ApiClient::class.java)
-        }
-    }
 }
